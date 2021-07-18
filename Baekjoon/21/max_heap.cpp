ce5698c031982533f9bfec854b290c035020f967
Luigi38 <71323379+Luigi38@users.noreply.github.com>
2021-07-18 16:50:24 +0900

    Add files via upload

---
+++ Baekjoon/21/max_heap.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	priority_queue<int> pq;
+	vector<int> v;
+
+	for (int i = 0; i < N; i++) {
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (pq.empty()) {
+				v.push_back(0);
+				continue;
+			}
+
+			int out = pq.top();
+			pq.pop();
+
+			v.push_back(out);
+		}
+		else {
+			pq.push(x);
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << '\n';
+	}
+
+	return 0;
+}
